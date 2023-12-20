d900a03f1760a0ddb534cdcbcff87898d43f8d36
Nerko <nermin.jasarevicg@outlook.com>
2023-12-21 00:38:06 +0100

    test KV

--- devops/allow_access_to_KV_for_host.sh
+++ devops/allow_access_to_KV_for_host.sh
@@ -6,7 +6,7 @@ KEY_VAULT_SUBSCRIPTION_ID=$2
 host_ip=$(curl https://api.ipify.org)
 echo Host IP is: $host_ip
 
-# az account set --name $KEY_VAULT_SUBSCRIPTION_ID
-# az keyvault update --name $KEY_VAULT_NAME --public-network-access Enabled --default-action Deny
-# az keyvault network-rule add --name $KEY_VAULT_NAME --ip-address $host_ip
-# echo Access for host IP $host_ip granted
+az account set --name $KEY_VAULT_SUBSCRIPTION_ID
+az keyvault update --name $KEY_VAULT_NAME --public-network-access Enabled --default-action Deny
+az keyvault network-rule add --name $KEY_VAULT_NAME --ip-address $host_ip
+echo Access for host IP $host_ip granted
