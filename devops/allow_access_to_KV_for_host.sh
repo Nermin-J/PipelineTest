89ce4034b9b71a52e1d47b893d8639f2bdc9f178
Nerko <nermin.jasarevicg@outlook.com>
2023-12-20 23:45:03 +0100

    add pipeline

---
+++ devops/allow_access_to_KV_for_host.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+KEY_VAULT_NAME=$1
+KEY_VAULT_SUBSCRIPTION_ID=$2
+
+host_ip=$(curl https://api.ipify.org)
+echo Host IP is: $host_ip
+
+az account set --name $KEY_VAULT_SUBSCRIPTION_ID
+az keyvault update --name $KEY_VAULT_NAME --public-network-access Enabled --default-action Deny
+az keyvault network-rule add --name $KEY_VAULT_NAME --ip-address $host_ip
+echo Access for host IP $host_ip granted
---
+++ devops/disallow_access_to_KV_for_host.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+KEY_VAULT_NAME=$1
+KEY_VAULT_SUBSCRIPTION_ID=$2
+
+az account set --name $KEY_VAULT_SUBSCRIPTION_ID
+az keyvault update --name $KEY_VAULT_NAME --public-network-access Disabled --default-action Deny
+az keyvault update --name $KEY_VAULT_NAME --set properties.networkAcls.ipRules=[]
+echo Access for host IP revoked
