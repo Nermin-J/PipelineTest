07518af9dd1dfa272de8f479d1d736fdca52f955
Nerko <nermin.jasarevicg@outlook.com>
2023-12-21 01:48:41 +0100

    supress output

--- devops/disallow_access_to_KV_for_host.sh
+++ devops/disallow_access_to_KV_for_host.sh
@@ -5,4 +5,4 @@ KEY_VAULT_SUBSCRIPTION_ID=$2
 
 az account set --name $KEY_VAULT_SUBSCRIPTION_ID --output none
 az keyvault update --name $KEY_VAULT_NAME --public-network-access Disabled --default-action Deny --set properties.networkAcls.ipRules=[] --output none
-echo Access to key vault \"$KEY_VAULT_NAME\" for host IP revoked
+echo Access to key vault \"$KEY_VAULT_NAME\" revoked for all IPs
--- devops/disallow_access_to_storage_account_for_host.sh
+++ devops/disallow_access_to_storage_account_for_host.sh
@@ -5,4 +5,4 @@ STORAGE_ACCOUNT_SUBSCRIPTION_ID=$2
 
 az account set --name $STORAGE_ACCOUNT_SUBSCRIPTION_ID --output none
 az storage account update --name $STORAGE_ACCOUNT_NAME --public-network-access Disabled --default-action Deny --set networkRuleSet.ipRules=[] --output none
-echo Access to storage account \"$STORAGE_ACCOUNT_NAME\" for host IP revoked
+echo Access to storage account \"$STORAGE_ACCOUNT_NAME\" revoked for all IPs
