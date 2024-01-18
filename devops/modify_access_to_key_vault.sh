9f1d43f50b7b0b24e6cf81ee974dcc63fd63a61b
Nerko <nermin.jasarevicg@outlook.com>
2024-01-18 16:35:40 +0100

    test new allow kv 222

--- devops/modify_access_to_key_vault.sh
+++ devops/modify_access_to_key_vault.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ####################################################################################################
 # This script allows/revokes access to key vault based on the input parameter (allow/revoke)
